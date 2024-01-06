c969ab4b736d89408d71b97be663ada88df1823e
Ruan Failache <ruanfailache@gmail.com>
2024-01-06 12:29:38 -0300

    build: install eslint and prettier

---
+++ src/app/pages/countries-list/countries-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-countries-list",
+	standalone: true,
+	imports: [],
+	templateUrl: "./countries-list.component.html",
+	styleUrl: "./countries-list.component.scss",
+})
+export class CountriesListComponent {}
